7f6f90e5b312eb2b353f642c79e30061f6f32f5c
Ramy Hakam <pencilsoft1@gmail.com>
2019-08-03 13:51:58 +0200

    adding routing and creae test for routes

---
+++ tests/Units/RoutesTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Auther:RamyHakam
+ * Date: 8/3/19
+ */
+
+namespace Tests\units;
+
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\RouteCollection;
+
+
+class RoutesTest extends TestCase
+{
+    /**
+     * @var RouteCollection $routes
+     */
+    private $routes;
+
+    /**
+     * init the list of routes
+     */
+    protected function setUp(): void
+    {
+        $this->routes = include __DIR__ . '/../../src/router.php';
+
+    }
+
+    /**
+     * Assert if All Routes has the same file names
+     */
+    public function testIFAllRoutesExist()
+    {
+        foreach ($this->routes->all() as $key => $route) {
+            $this->assertFileExists(__DIR__."/../../src/pages/{$key}" . ".php");
+        }
+    }
+}
--- web/index.php
+++ web/index.php
@@ -7,24 +7,50 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
 
+/**
+ * create a new Request if it is not passed from other files like test
+ * @var Request $request
+ */
 $request = $request? $request : Request::createFromGlobals();
 
-$map = [
-    '/hello' => 'helloWorld',
-];
+/**
+ * contain the routes list
+ * @var  RouteCollection $routes
+ */
+$routes = include __DIR__ . '/../src/router.php';
 
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
-    ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
-    $response= new Response(ob_get_clean());
-} else {
-     $response = new Response('Not Found',404);
-}
+/**
+ * contain the Request info
+ * @var  RequestContext $context
+ */
+$context = new RequestContext();
 
+//get the context from the request
+$context->fromRequest($request);
+
+/**
+ * match the request url with the router list
+ * @var  UrlMatcher $matcher
+ */
+$matcher = new UrlMatcher($routes, $context);
+
+//import the list variables from the matcher result
+extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+
+//start internal buffering
+ob_start();
+
+//get the routing file
+include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+//create the response form buffering data
+$response = new Response(ob_get_clean());
+
+//send the response
 $response->send();
